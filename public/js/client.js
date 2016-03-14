9c77357e56d1660bc62ba9bac5e06999eff89d70
Lise Muller <lisemuller@hotmail.fr>
2016-03-14 15:23:28 +0000

    authentications

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,6 @@
 $(function(){
   console.log("Jake Weary at your service");
+  $('form').on('submit', submitForm);
   createMap();
 });
 
@@ -12,3 +13,68 @@ function createMap(){
     disableDefaultUI: true
   });
 }
+
+
+////// AUTHENTICATIONS REQUEST ////////
+
+function submitForm(){
+  // get the data from the forms and make an ajaxRequest
+  // call authenticationSuccessful
+  event.preventDefault(); // not to reload the page with the form 
+
+  var form = this; // to clear the form
+
+  var method = $(this).attr('method'); // attribute to the form the right methode
+  var url = "http://localhost:3000/api" + $(this).attr('action'); //post to this url and do this action
+  var data = $(this).serialize(); // we don't use json because we have put url encoded in our app.js // the data sort like name=Mike&email=mike.hayden@ga.co
+
+  form.reset(); // to clear the form
+  ajaxRequest(method, url, data, authenticationSuccessful);
+  }
+
+
+function authenticationSuccessful(data) {
+    // set the token and call checkLoginState
+    if(data.token) setToken(data.token);
+
+  }
+
+function setToken(token) {
+    // set the token into localStorage
+    return localStorage.setItem('token', token);
+
+  }
+
+function getToken() {
+    // get the token from localStorage
+    return localStorage.getItem('token');
+  }
+
+
+function ajaxRequest(method, url, data, callback) {
+    // create a re-useable ajaxRequest function
+      return $.ajax({
+        method: method,
+        url: url,
+        data: data,
+        beforeSend: function(jqXHR, settings) {
+          var token = getToken();
+          if(token) return jqXHR.setRequestHeader('Authorization', 'Bearer ' + token);
+        }
+      })
+      .done(callback)
+      .fail(function(err) {
+      console.error(err);
+    });
+}
+
+function logout(){
+  // remove the token
+  removeToken();
+}
+
+function removeToken() {
+    // remove the token from localStorage
+    return localStorage.removeItem('token');
+}
+
