bbb280844831d35eade4cbe06336c760b21bb28b
maxhilliard <mx.hilliard@gmail.com>
2016-03-14 14:37:54 +0000

    Map loading. Some extra styling to make map appear properly

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,14 @@
 $(function(){
   console.log("Jake Weary at your service");
+  createMap();
 });
+
+function createMap(){
+  // Make a new map
+  map = new google.maps.Map($('#map')[0], {
+    // Pass in parameters to the map
+    zoom: 12,
+    center: { lat: 51.506178, lng: -0.088369 },
+    disableDefaultUI: true
+  });
+}
